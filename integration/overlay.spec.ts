55dc47402b558ad5a10a5f5bba3cadfa051130c4
Sim Boon Long <simboonlong@gmail.com>
2022-09-21 22:50:27 +0800

    feat: overlay menu (#17)

--- integration/overlay.spec.ts
+++ integration/overlay.spec.ts
@@ -2,24 +2,29 @@ import { Selector } from "testcafe";
 
 fixture`Overlay`.page("http://localhost:3000/overlay");
 
-test("should show overlay when clicked on trigger", async (t) => {
-  await t.click(".overlay-trigger");
+test("should show overlay (lightbox) when clicked on trigger", async (t) => {
+  await t.click(".overlay-trigger[data-trigger='lightbox']");
   await t.expect(Selector(".overlay").visible).ok();
 });
 
-test("should close overlay when clicked on cancel", async (t) => {
-  await t.click(".overlay-trigger");
-  await t.click(".overlay-cancel");
+test("should close overlay (lightbox) when clicked on cancel", async (t) => {
+  await t.click(".overlay-trigger[data-trigger='lightbox']");
+  await t.click(".overlay-lightbox .overlay-cancel");
   await t.expect(Selector(".overlay").visible).notOk();
 });
 
-test("should close overlay when clicked on overlay background", async (t) => {
-  await t.click(".overlay-trigger");
-  await t.click(".overlay-background", { offsetX: 0, offsetY: 0 });
+test("should close overlay (lightbox) when clicked on overlay background", async (t) => {
+  await t.click(".overlay-trigger[data-trigger='lightbox']");
+  await t.click(".overlay-lightbox .overlay-background", {
+    offsetX: 0,
+    offsetY: 0,
+  });
   await t.expect(Selector(".overlay").visible).notOk();
 });
 
-test("should update overlay content", async (t) => {
+test("should update overlay (lightbox) content", async (t) => {
   await t.click("#foobar");
-  await t.expect(Selector(".overlay-content").textContent).contains("foobar");
+  await t
+    .expect(Selector(".overlay-lightbox .overlay-content").textContent)
+    .contains("foobar");
 });
--- src/overlay/index.ts
+++ src/overlay/index.ts
@@ -1,24 +1,65 @@
-export const overlayUpdateContent = (rawHtml: string): void => {
-  const content = <HTMLElement>document.querySelector(".overlay-content");
-  content.innerHTML = rawHtml;
-};
+const ALL_TYPES = ["lightbox", "menu"] as const;
+type typeTuple = typeof ALL_TYPES;
+type overlayType = typeTuple[number];
 
-export const overlayOpen = (): void => {
-  document.querySelector(".overlay")?.classList.add("is-active");
-};
+interface Overlay {
+  overlay: HTMLElement;
+  trigger: HTMLElement[];
+}
 
-export const overlayClose = (): void => {
-  document.querySelector(".overlay")?.classList.remove("is-active");
-};
+interface OverlayHelper {
+  updateContent: (rawHtml: string) => void;
+  open: (type: overlayType) => void;
+  close: () => void;
+}
 
-export const overlay = (): void => {
-  document
-    .querySelector(".overlay-trigger")
-    ?.addEventListener("click", overlayOpen);
+export const overlay = ({
+  overlay,
+  trigger,
+}: Overlay): OverlayHelper | undefined => {
+  const updateContent = (rawHtml: string): void => {
+    const content = <HTMLElement>(
+      document.querySelector(".overlay-lightbox .overlay-content")
+    );
+    content.innerHTML = rawHtml;
+  };
+
+  const open = (type: overlayType): void => {
+    overlay.classList.add("is-active");
+    overlay.setAttribute("data-trigger", type);
+  };
+
+  const close = (): void => {
+    overlay.classList.remove("is-active");
+    overlay.removeAttribute("data-trigger");
+  };
+
+  const onOpenHandler = (el: HTMLElement) => {
+    const type = el.getAttribute("data-trigger") as overlayType;
+
+    if (!type) {
+      console.error(
+        `data-trigger attribute is not set on trigger element. it only accepts "${ALL_TYPES}".`,
+      );
+      return;
+    }
+
+    el.addEventListener("click", () => {
+      open(type);
+    });
+  };
+
+  if (trigger.length > 0) {
+    trigger.forEach((el) => onOpenHandler(el));
+  } else {
+    onOpenHandler(trigger as unknown as HTMLElement);
+  }
 
   document
     .querySelectorAll(".overlay-background, .overlay-cancel")
     .forEach((el) => {
-      el.addEventListener("click", overlayClose);
+      el.addEventListener("click", close);
     });
+
+  return { updateContent, open, close };
 };
